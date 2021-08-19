6ae0bcf0e151c49cfac25b2d2283fecec23dbfe9
mayank0508 <thakurmayank0508@gmail.com>
2021-08-19 14:00:17 +0530

    Day 11 button

--- Projects/#11 Video Player/scripts.js
+++ Projects/#11 Video Player/scripts.js
@@ -11,17 +11,22 @@ const ranges = player.querySelector('.player__slider');
 // Build function
 const togglePlay = () => {
   // this function helps in pauseing and playing  the game
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  //   if (video.paused) {
+  //     video.play();
+  //   } else {
+  //     video.pause();
+  //   }
+
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
 };
 
-
-const updateButton = () => { // this function helps in updating the button 
-    console.log('update the Buttons');
-  }; 
+const updateButton = () => {
+  // this function helps in updating the button
+  const icon = this.paused ? 'A' : 'B';
+  console.log(icon);
+  toggle.textContent = icon;
+};
 
 // Hoooks up the event listeners
 video.addEventListener('click', togglePlay);
