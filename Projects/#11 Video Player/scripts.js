177d142dd38d33edd7da12f23d226f125f648408
mayank0508 <thakurmayank0508@gmail.com>
2021-08-19 12:26:54 +0530

    Day 11 selecting elements done

---
+++ Projects/#11 Video Player/scripts.js
@@ -0,0 +1,12 @@
+//Get our elements
+const players = document.querySelectorAll('.player');
+const video = players.querySelectorAll('.viewer');
+const progress = players.querySelectorAll('.progress');
+const progressBar = players.querySelector('.progress__filled');
+
+const toggle = player.querySelectorAll('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+// Build function
+
+// Hoooks up the event listeners
